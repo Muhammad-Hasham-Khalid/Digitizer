e8ae9962a7be25124baaffd63f221b94b974e7db
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-19 14:21:53 +0500

    Complete V1 of the app
    Co-authored-by: Midha Tahir <MidhaTahir@users.noreply.github.com>
    Co-authored-by: Muhammad Hasham <Muhammad-Hasham-Khalid@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
+from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=['GET', 'POST'])
+model = load_model("./model/trained_model.h5")
+
+@app.route("/", methods=['POST'])
 def index():
     if request.method == 'POST':
-        image = request.files['image'].read()
-        print(image)
-        return "POST"
-    else:
-        return "GET"
+        image = Image.open(request.files['image']).convert('L')
+        npImage = np.array(image)
+        res = np.argmax(model.predict(npImage.reshape(-1, 784)))
+        return f"Result : {res}"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
