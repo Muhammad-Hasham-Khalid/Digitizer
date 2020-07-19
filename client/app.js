5b7ad94b9aa405c5df37fe8378d20dfad963afbf
Midha <midhatahirkhan2011@gmail.com>
2020-07-19 15:41:58 +0500

    Add interface 	Co-authored-by: Midha Tahir <MidhaTahir@users.noreply.github.com> 	Co-authored-by: Muhammad Hasham <Muhammad-Hasham-Khalid@users.noreply.github.com>

--- client/app.js
+++ client/app.js
@@ -15,7 +15,21 @@ async function fetchResult(e) {
     });
     const res = await data.text();
     console.log(res);
+    uiAlert("Image Uploaded", "alert alert-light");
   } catch (error) {
     console.log(error);
+    uiAlert("Image Not Uploaded", "alert alert-danger");
   }
+}
+
+function uiAlert(message, className) {
+  let div = document.createElement("div");
+  div.className = className;
+  div.appendChild(document.createTextNode(message));
+  let container = document.querySelector(".uicontainer");
+  let parent = document.querySelector("#form");
+  container.insertBefore(div, parent);
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
 }
