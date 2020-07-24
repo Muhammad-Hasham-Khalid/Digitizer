ef4aca6adf9beb7af7651a6e4aa0653381ebb1b8
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-24 11:48:16 +0500

    Finalize and Clean code⭐⭐
    Co-authored-by: Midha Tahir <MidhaTahir@users.noreply.github.com>
    Co-authored-by: Muhammad Hasham <Muhammad-Hasham-Khalid@users.noreply.github.com>

--- app.py
+++ app.py
@@ -8,33 +8,23 @@ import os, json
 app = Flask(__name__)
 CORS(app)
 
-model_path = os.path.join(os.getcwd(), 'model', 'trained_model.h5')
+model_path = os.path.join(os.getcwd(), 'model', 'mnist_model.h5')
 model = load_model(model_path)
 
 @app.route("/", methods=['POST'])
 def index():
     if request.method == 'POST':
         try:
-            # image = Image.open(request.files.get('image-file')).convert('L')
             pixels = np.array(json.loads(request.form.get('pixels')).get('values'))
-            # image operations
-            # newsize = (28, 28) 
-            # image = image.resize(newsize) 
-            # npImage = np.array(image)
-            # npImage = npImage.astype('float32') / 255
 
             # pixels operations
-            pixels = Image.fromarray(pixels.reshape(400, 400, 4).astype(np.uint8)).convert('L')
-            # pixels = np.resize(pixels, (28, 28))
-            pixels = np.array(pixels.resize((28, 28))).astype('float32') / 255
-            print(pixels.shape)
+            tempImage = Image.fromarray(pixels.reshape(400, 400, 4).astype(np.uint8)).convert('L')
+            pixels = np.array(tempImage.resize((28, 28))).astype('float32') / 255
             res = np.argmax(model.predict(pixels.reshape(-1, 784)))
-            # res = 1
             return f"{res}"
         except Exception as e:
             print(e)
             return "Error"
-            
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- client/sketch.js
+++ client/sketch.js
@@ -1,17 +1,80 @@
+let pixelData = null;
+let isDrawn = false;
+
 function setup() {
   const canvas = createCanvas(400, 400);
-  eraser = createButton("clear");
+  cursor(CROSS);
+
+  btnContainer = createDiv();
+  btnContainer.addClass("container text-center p-3");
+
+  // clearing the canvas
+  eraser = createButton("Clear");
+  eraser.id("eraser");
   eraser.mousePressed(changeBG);
-  eraser.addClass("btn btn-primary btn-lg clearButton");
-  // saver = createButton("Saving Image");
-  // saver.mousePressed(convertToImage);
+  eraser.addClass("btn btn-primary btn-lg customButton");
+
+  // checking the result
+  submit = createButton("Check");
+  submit.id("submit");
+  submit.mousePressed(fetchResult);
+  submit.addClass("btn btn-light btn-lg customButton");
+
+  btnContainer.child("eraser");
+  btnContainer.child("submit");
+
   background(0);
   colorMode(RGB);
+}
 
-  // erasing the canvas
-  function changeBG() {
-    background(0);
+async function fetchResult(e) {
+  convertToImage();
+  const fData = new FormData();
+  if (isDrawn) {
+    fData.append("pixels", pixelData);
+    try {
+      let data = await fetch("http://127.0.0.1:5000/", {
+        method: "POST",
+        body: fData,
+      });
+      const res = await data.text();
+      if (res != "Error") {
+        Swal.fire({
+          icon: "success",
+          title: "Result",
+          text: `The number is predicted to be: ${res}`,
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...Try again",
+          text: res,
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...Try again",
+        text: error,
+      });
+    }
+    changeBG();
+  } else {
+    uiAlert("Please draw something on the canvas", "alert alert-warning");
   }
+}
+
+// custom alert
+function uiAlert(message, className) {
+  let div = document.createElement("div");
+  div.className = className;
+  div.appendChild(document.createTextNode(message));
+  let container = document.querySelector(".uicontainer");
+  let parent = document.querySelector("#form");
+  container.insertBefore(div, parent);
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
 }
 
 // saving canvas function
@@ -19,14 +82,20 @@ function convertToImage() {
   loadPixels();
   pixelData = Array.from(pixels);
   pixelData = JSON.stringify({ values: pixelData });
-  console.log(pixelData);
   updatePixels();
 }
 
 function mouseDragged() {
+  isDrawn = true;
   stroke(color(255));
   strokeWeight(20);
   if (mouseX < 500) {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
+}
+
+// erasing the canvas
+function changeBG() {
+  isDrawn = false;
+  background(0);
 }
